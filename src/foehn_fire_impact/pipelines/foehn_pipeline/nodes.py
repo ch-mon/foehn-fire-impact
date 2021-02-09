314983e6c6a4f8be1b278eb6b25e9aafbc487988
Chris Mony <chmony@student.ethz.ch>
2021-02-09 20:49:38 +0100

    Implemented basic code for older foehn data reading

--- src/foehn_fire_impact/pipelines/foehn_pipeline/nodes.py
+++ src/foehn_fire_impact/pipelines/foehn_pipeline/nodes.py
@@ -34,8 +34,7 @@ def load_comprehensive_foehn_data(df_meteo_params, parameters):
             final_variable_name = f"{station_abbr}_{meteo_param_mapping[parameter]}"
 
             # Read file
-            df = pd.read_csv(os.path.join(root, file), delimiter=";", usecols=["termin", parameter]).rename(
-                columns={"termin": "date", parameter: final_variable_name})
+            df = pd.read_csv(os.path.join(root, file), delimiter=";", usecols=["termin", parameter], encoding='latin1').rename(columns={"termin": "date", parameter: final_variable_name})
 
             # If dataframe does not contain rows, continue
             if len(df.index) == 0:
@@ -70,6 +69,52 @@ def load_comprehensive_foehn_data(df_meteo_params, parameters):
     df_final.reset_index(drop=True, inplace=True)
 
     return df_final
+
+def load_older_data(parameters):
+
+    df_final = pd.DataFrame(index=pd.date_range(start='1980-01-01 00:00', end='2019-12-31 23:59', freq="10min"))
+
+    # Read in North foehn data
+    north_foehn_list = os.listdir(parameters["older_north_foehn_data_path"])
+
+    for location in north_foehn_list:
+        logging.info(location)
+        df = pd.read_csv(os.path.join(parameters["older_north_foehn_data_path"], location),
+                         header=0,
+                         names=["date", "foehn"])
+        df["date"] = pd.to_datetime(df["date"], format="%Y%m%d%H%M")
+        df.columns = ["date", location[0:3] + "_foehn"]
+        df.dropna(subset=["date"], inplace=True)
+        df_final = df_final.merge(df, on="date", how="left", validate="one_to_one")
+
+    # TODO: Combine the MAE and MAW station into one MAG column
+    MAG_dummy = df_final["MAE_foehn"].copy()
+    MAG_dummy_mask = (df_final["MAW_foehn"] == 1.0)
+    MAG_dummy.loc[MAG_dummy_mask] = 1.0
+    MAG_dummy_mask = (df_final["MAE_foehn"] == 0.0) & (df_final["MAW_foehn"].isnull())
+    MAG_dummy.loc[MAG_dummy_mask] = np.NaN
+
+    df_final.loc[df_final["date"].dt.year < 2017, "MAG_foehn"] = MAG_dummy.loc[
+        df_final["date"].dt.year < 2017].values
+
+    # TODO: Combine the OTE and OTW station into one OTL column
+    df_final["OTL_foehn"] = np.NaN
+    OTL_dummy = df_final["OTE_foehn"].copy()
+    OTL_dummy_mask = (df_final["OTW_foehn"] == 1.0)
+    OTL_dummy.loc[OTL_dummy_mask] = 1.0
+    OTL_dummy_mask = (df_final["OTE_foehn"] == 0.0) & (df_final["OTW_foehn"].isnull())
+    OTL_dummy.loc[OTL_dummy_mask] = np.NaN
+
+    df_final.loc[df_final["date"].dt.year < 2017, "OTL_foehn"] = OTL_dummy.loc[
+        df_final["date"].dt.year < 2017].values
+
+    df_final.drop(["MAW_foehn", "MAE_foehn", "OTW_foehn", "OTE_foehn"], inplace=True, axis=1)
+
+    return df_final
+
+
+def merge_old_and_new_foehn_data(df_new):
+    ...
 
 def cleanse_foehn_data(df):
 
