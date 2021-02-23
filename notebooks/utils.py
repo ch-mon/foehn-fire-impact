6c1f9cbd1b5e3fb60dee8d273f9805e9b4c9a7fe
Chris Mony <chmony@student.ethz.ch>
2021-02-23 13:42:35 +0100

    Transferred all functions to python file

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -72,10 +72,41 @@ def test_foehn_within_variable(df, hours, control_var, categories):
             foehn_values_2 = df.loc[
                 (df[control_var] == category_dummy) & (intervals == pd.Interval(0.001, 60 * hours)), "total [ha]"]
 
-            print(f"({hours}h) ", category, " vs. " , category_dummy, "\t",
+            print(f"({hours}h) ", category, " vs. ", category_dummy, "\t",
                   np.round(sci.stats.ranksums(foehn_values_1, foehn_values_2).pvalue, 6), "\t",
                   np.round(foehn_values_1.median() / foehn_values_2.median(), 3))
 
+def test_foehn_strength(df, hours, strength_var, bins):
+    foehn_mask = df[f'foehn_minutes_during_{hours}_hours_after_start_of_fire'] > 0
+    print(foehn_mask.sum())
+    df = df.loc[foehn_mask, :].reset_index(drop=True)
+
+    strength_col = f"{strength_var}_mean_during_{hours}_hours_after_start_of_fire"
+    #df[strength_col].hist(alpha=0.5, bins=30)
+
+    plt.figure()
+
+    g = sns.boxplot(x=pd.cut(df[strength_col], bins=bins), y=df["total [ha]"], color="tab:blue")
+
+    plt.xlabel(f"{strength_var} [km/h]")
+    plt.ylabel("Total burned area [ha]")
+    g.set_yscale("log")
+    plt.grid(True, which="both", ls="--", c='gray', alpha=0.5)
+    save_figure(f"BoxplotBurnedAreaOver{strength_var}ForFirst{hours}HoursAfterStart")
+
+    intervals = pd.cut(df[strength_col], bins=bins).unique().sort_values(ascending=True).to_list()
+    intervals_dummies = intervals.copy()
+    for interval in intervals:
+        intervals_dummies.remove(interval)
+        burned_values_1 = df.loc[(pd.cut(df[strength_col], bins=bins) == interval), "total [ha]"]
+        for interval_dummy in intervals_dummies:
+            burned_values_2 = df.loc[(pd.cut(df[strength_col], bins=bins) == interval_dummy), "total [ha]"]
+
+            print(f"({hours}h) ", interval, " vs. ", interval_dummy, "\t",
+                  np.round(sci.stats.ranksums(burned_values_1, burned_values_2).pvalue, 6), "\t",
+                  np.round(burned_values_2.median() / burned_values_1.median(), 3))
+
+    return
 
 def plot_multiple_binned_burned_area_after_fire_start(df, hours, control_var=""):
     """
@@ -144,52 +175,56 @@ def plot_binary_binned_burned_area_after_fire_start(df, hours, control_var=""):
     save_figure(f"NonVsFoehnBurnedAreaOverFoehnMinutesForFirst{hours}HoursAfterStart")
     return
 
-def plot_binned_burned_area_before_fire_start(df, hours):
-    bin_amount=6
-    amount_of_fires = df[f'foehn_minutes_{hours}_hour_before'].count()
-    df_binned = df.groupby(pd.cut(df[f'foehn_minutes_{hours}_hour_before'], bins=bin_amount)).count()
+def plot_binned_burned_area_before_fire_start(df, df_foehn, hours):
+    for bin_amount in [6,[-0.001,0.001,hours*60]]:
+        df_binned = df.groupby(pd.cut(df[f'foehn_minutes_{hours}_hour_before'], bins=bin_amount)).count()
 
-    foehn_minutes_in_interval = np.zeros(bin_amount)
-    foehn_stations = df.columns.tolist()
-    foehn_stations.remove("date")
+        foehn_minutes_in_interval = np.zeros(len(df_binned.index))
+        for station in df_foehn:
+            foehn_minutes_during_timeframe = df_foehn[station].rolling(6*hours).sum()*10
+            foehn_minutes_in_interval += np.array([((i.left < foehn_minutes_during_timeframe) &
+                                                     (foehn_minutes_during_timeframe <= i.right)).sum() for i in df_binned.index])
 
-    for station in foehn_stations:
-        foehn_minutes_during_timeframe = df[station].rolling(6*hours).sum()*10
-        foehn_minutes_in_interval += np.array([((df_binned.index[i].left < foehn_minutes_during_timeframe) &
-                                                 (foehn_minutes_during_timeframe <df_binned.index[i].right)).sum() for i in range(len(df_binned.index))])
+        df_binned["foehn_minutes_in_interval"] =foehn_minutes_in_interval/len(df_foehn.columns)
+        df_binned["normalized_count"] = df_binned['total [ha]'] / df_binned["foehn_minutes_in_interval"]
 
-    foehn_minutes_in_interval = foehn_minutes_in_interval/len(foehn_stations)
-    df_binned["foehn_minutes_in_interval"] =foehn_minutes_in_interval
+        plt.figure()
+        sns.barplot(x=df_binned.index, y=df_binned["normalized_count"]/df_binned["normalized_count"][0], color="tab:blue")
+        plt.ylabel("Normalized count of fires")
+        plt.xlabel("")
+        xticks, xlabels = plt.xticks()
+        xlabels = [label.get_text() for label in xlabels]
+        if bin_amount==6:
+            plt.xlabel("Foehn minutes")
+            xlabels[0] = "[0.0" + xlabels[0][6:]
+            figname=f"NormalizedFireCountOverFoehnMinutesForThe{hours}HoursBeforeStart"
+        else:
+            xlabels[0] = "Non-foehn fires"
+            xlabels[1] = "Foehn fires"
+            figname = f"NonFoehnVsFoehnOverFoehnMinutesForThe{hours}HoursBeforeStart"
 
+        plt.xticks(xticks, xlabels)
+        save_figure(figname)
+
+def plot_binned_burned_area_before_fire_start_temperature(df, df_foehn, hours, stations):
+    foehn_mask = (df[f'foehn_minutes_{hours}_hour_before'] > 0) #& (df["potential_foehn_species"] == "North foehn")
+    df = df.loc[foehn_mask, :]
+
+    bins = [0, 3, 6, 9, 12, 15]
+    df_binned = df.groupby(pd.cut(df[f"TT_mean_{hours}_hour_before"], bins=bins)).count()
+
+    foehn_conditions_in_interval = np.zeros(len(df_binned.index))
+    for station in stations:
+        temp_mean_foehn = df_foehn[f"{station}_TT"].where(df_foehn[f"{station}_foehn"] == 1.0).rolling(6 * hours, min_periods=1).mean()
+        temp_mean_no_foehn = df_foehn[f"{station}_TT"].where(df_foehn[f"{station}_foehn"] == 0.0).rolling(6 * hours, min_periods=1).mean()
+        strength_difference = temp_mean_foehn - temp_mean_no_foehn
+
+        foehn_conditions_in_interval += np.array([((i.left < strength_difference) &
+                                                   (strength_difference < i.right)).sum() for i in df_binned.index])
+
+    df_binned["normalized_count"] = df_binned['total [ha]'] / foehn_conditions_in_interval
     plt.figure()
-    sns.barplot(x=df_binned.index, y=df_binned['total [ha]']/df_binned["foehn_minutes_in_interval"], color="tab:blue")
+    sns.barplot(x=df_binned.index, y=df_binned["normalized_count"]/df_binned["normalized_count"][0], color="tab:blue")
     plt.ylabel("Normalized count of fires")
-    plt.xlabel("Foehn minutes")
-    xticks, xlabels = plt.xticks()
-    xlabels = [label.get_text() for label in xlabels]
-    xlabels[0] = "[0.0" + xlabels[0][6:]
-    plt.xticks(xticks, xlabels)
-    save_figure(f"NormalizedFireCountOverFoehnMinutesForThe{hours}HoursBeforeStart")
-
-    amount_of_fires = df[f'foehn_minutes_{hours}_hour_before'].count()
-    df_binned = df.groupby(pd.cut(df[f'foehn_minutes_{hours}_hour_before'], bins=[-0.001,0.001,hours*60])).count()
-    foehn_minutes_in_interval = np.zeros(2)
-    foehn_stations = df.columns.tolist()
-    foehn_stations.remove("date")
-
-    for station in foehn_stations:
-        foehn_minutes_during_timeframe = df[station].rolling(6*hours).sum()*10
-        foehn_minutes_in_interval += np.array([((df_binned.index[i].left < foehn_minutes_during_timeframe) &
-                                                 (foehn_minutes_during_timeframe <df_binned.index[i].right)).sum() for i in range(len(df_binned.index))])
-
-    df_binned["foehn_minutes_in_interval"] =foehn_minutes_in_interval/len(foehn_stations)
-    plt.figure(figsize=(16,9))
-    sns.barplot(x=df_binned.index, y=df_binned['total [ha]']/df_binned["foehn_minutes_in_interval"], color="tab:blue")
-    plt.ylabel("Normalized count of fires")
-    plt.xlabel("")
-    xticks, xlabels = plt.xticks()
-    xlabels = [label.get_text() for label in xlabels]
-    xlabels[0] = "Non-Foehn fires"
-    xlabels[1] = "Foehn fires"
-    plt.xticks(xticks, xlabels)
-    save_figure(f"NonFoehnVsFoehnOverFoehnMinutesForThe{hours}HoursBeforeStart")
+    plt.xlabel("Foehn temperature increase [K]")
+    save_figure(f'NormalizedFireCountOverTemperatureForThe{hours}HoursBeforeStart')
